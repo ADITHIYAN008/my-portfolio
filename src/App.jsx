3248b547448b3e52a84683b25b34b7a00c7395a8
Adithiyan <adithiyanr2@gmail.com>
2024-12-22 10:22:04 +0530

    Sixth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,6 @@
-import { useEffect, useState } from "react";
 import NavBar from "./components/NavBar";
-import Loading from "./components/Loading";
 
 const App = () => {
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setIsLoading(false);
-  //   }, 2000);
-  // }, []);
-
   return (
     <>
       <NavBar />
